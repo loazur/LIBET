33829da4ff4390ed809a8582e5399854b907b7f3
loazur <clement.bounaix@gmail.com>
2025-11-04 10:12:59 +0100

    Probleme avec le drop

--- Assets/Scripts/Player/InteractionSystem/InteractionObjects/S_ItemInteraction.cs
+++ Assets/Scripts/Player/InteractionSystem/InteractionObjects/S_ItemInteraction.cs
@@ -7,11 +7,14 @@ public class S_ItemInteraction : MonoBehaviour, SI_Interactable
     [Header("Gestion de l'item")]
     [SerializeField] private S_PlayerInteract playerInteract;
     [SerializeField] private string interactText; // Nom de l'objet
+    private Rigidbody rigidbodyItem;
     private InputAction interactAction;
+
 
     void Start()
     {
         interactAction = InputSystem.actions.FindAction("Interact");
+        rigidbodyItem = GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -46,23 +49,34 @@ public class S_ItemInteraction : MonoBehaviour, SI_Interactable
             return;
         }
 
-        //! Ramasser
-        Debug.Log("Attraper " + interactText);
+        rigidbodyItem.useGravity = false;
+        rigidbodyItem.isKinematic = true;
+        rigidbodyItem.constraints = RigidbodyConstraints.FreezeRotation;
+        transform.SetParent(playerInteract.transform);
+
         playerInteract.DisableInteractions();
         playerInteract.setHoldingItem(true);
     }
 
     private void HoldingItem() //& Gestion lorsqu'on tient un item
     {
-        if (playerInteract.isHoldingItem())
+        if (!playerInteract.isHoldingItem())
         {
-            if (interactAction.WasReleasedThisFrame())
-            {
-                Drop();
-            }
-
-            //! Gestion jeter
+            return;
         }
+
+        
+        Vector3 targetPos = playerInteract.transform.position + playerInteract.transform.forward * 1.3f;
+        transform.position = targetPos;
+        transform.rotation = playerInteract.transform.rotation;
+
+        if (interactAction.WasReleasedThisFrame())
+        {
+            Debug.Log("dd");
+            Drop();
+        }
+
+        //! Gestion jeter
     }
 
     private void Drop() //& Lacher un item
@@ -72,8 +86,14 @@ public class S_ItemInteraction : MonoBehaviour, SI_Interactable
             return;
         }
 
-        //! Lacher
-        Debug.Log("Lacher " + interactText);
+        //!marche po
+
+        rigidbodyItem.useGravity = true;
+        rigidbodyItem.isKinematic = false;
+        rigidbodyItem.constraints = RigidbodyConstraints.None;
+        transform.SetParent(null);
+        
+
         playerInteract.EnableInteractions();
         playerInteract.setHoldingItem(false);
     }
