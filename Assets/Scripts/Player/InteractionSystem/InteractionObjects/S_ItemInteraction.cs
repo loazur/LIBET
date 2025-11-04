e107143c8a806c0a35c41d2c90a715d2cf033514
loazur <clement.bounaix@gmail.com>
2025-11-04 09:34:13 +0100

    Avancement

--- Assets/Scripts/Player/InteractionSystem/InteractionObjects/S_ItemInteraction.cs
+++ Assets/Scripts/Player/InteractionSystem/InteractionObjects/S_ItemInteraction.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class S_ItemInteraction : MonoBehaviour, SI_Interactable
 {
     //~ Gestion de l'item
     [Header("Gestion de l'item")]
+    [SerializeField] private S_PlayerInteract playerInteract;
     [SerializeField] private string interactText; // Nom de l'objet
+    private InputAction interactAction;
+
+    void Start()
+    {
+        interactAction = InputSystem.actions.FindAction("Interact");
+    }
+
+    void Update()
+    {
+        HoldingItem();
+    }
+
 
     //! Méthodes provenant de l'interface SI_Interactable
 
-    public void Interact(Transform playerTransform)
+    public void Interact(Transform playerTransform) //& Ramasse l'item
     {
-        Debug.Log("Interagit avec objet!");
+        PickUp();
     }
 
     public string getInteractText()  //& Retourne le nom de l'item
@@ -25,18 +39,55 @@ public class S_ItemInteraction : MonoBehaviour, SI_Interactable
 
     //! --------------- Fonctions privés ---------------
 
-    private void PickUp()
+    private void PickUp() //& Ramasser un item
     {
+        if (playerInteract.isHoldingItem())
+        {
+            return;
+        }
 
+        //! Ramasser
+        Debug.Log("Attraper " + interactText);
+        playerInteract.DisableInteractions();
+        playerInteract.setHoldingItem(true);
     }
 
-    private void Drop()
+    private void HoldingItem() //& Gestion lorsqu'on tient un item
     {
+        if (playerInteract.isHoldingItem())
+        {
+            if (interactAction.WasReleasedThisFrame())
+            {
+                Drop();
+            }
 
+            //! Gestion jeter
+        }
+    }
+
+    private void Drop() //& Lacher un item
+    {
+        if (!playerInteract.isHoldingItem())
+        {
+            return;
+        }
+
+        //! Lacher
+        Debug.Log("Lacher " + interactText);
+        playerInteract.EnableInteractions();
+        playerInteract.setHoldingItem(false);
     }
     
-    private void Throw()
+    private void Throw() //& Lancer un item
     {
-        
+        if (!playerInteract.isHoldingItem())
+        {
+            return;
+        }
+
+        //! Lancer
+        Debug.Log("Jeter " + interactText);
+        playerInteract.EnableInteractions();
+        playerInteract.setHoldingItem(false);
     }
 }
