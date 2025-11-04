5c9230acde2ee037085390a54b32b5c5dc1086ba
Por-Tra <151206590+Por-Tra@users.noreply.github.com>
2025-11-04 10:03:10 +0100

    j'en peux plus du système pour s'assoir 😭

--- Assets/Scripts/Player/InteractionSystem/InteractionObjects/S_ChairInteractable.cs
+++ Assets/Scripts/Player/InteractionSystem/InteractionObjects/S_ChairInteractable.cs
@@ -4,23 +4,35 @@
 
 // TODO : faire une animation
 
-using System.Numerics;
+
 using UnityEngine;
 
 public class S_ChairInteractable : MonoBehaviour, SI_Interactable
 {
     private string interactText = "S'asseoir";
     [SerializeField] private GameObject player;
-
+    [SerializeField] private S_FirstPersonCamera playerCamera;
+    private S_PlayerController playerController;
     private bool isPlayerSitting = false;
 
+    // & ===================================================
+    
+    
+    
 
+
+    void Start()
+    {
+        playerController = player.GetComponent<S_PlayerController>();
+    }
+
+    // * ===================================================================================
     // * Ne pas retirer ce qui est en desssous, nécessaire pour l'interface SI_Interactable
     // * ===================================================================================
 
     // ~ Méthode qui est activer quand on interagit avec l'objet
     public void Interact(Transform playerTransform)
-    {   
+    {
         if (!isPlayerSitting)
         {
             isPlayerSitting = true;
@@ -46,7 +58,7 @@ public class S_ChairInteractable : MonoBehaviour, SI_Interactable
     // * ===================================================================================
 
     // Teleporte le joueur à la position assise
-    void SitPlayer()
+    private void SitPlayer()
     {
 
         // milieu de la chaise
@@ -56,23 +68,47 @@ public class S_ChairInteractable : MonoBehaviour, SI_Interactable
         player.transform.position = chairPosition_Center;
 
         // Bloquer les mouvements du joueur
-
+        playerController.DisableMovements();
+        CameraLock();
 
 
         interactText = "Se lever";
-        isPlayerSitting = true;
     }
 
-    void UnsitPlayer()
+    private void UnsitPlayer()
     {
 
         // Mettre le joueur debout à coté de la chaise
+        UnityEngine.Vector3 chairPosition_Side = transform.position + transform.right * 1.0f;
+        player.transform.position = chairPosition_Side;
 
 
         // Débloquer les mouvements du joueur
+        playerController.EnableMovements();
+        CameraLock();
+
 
         interactText = "S'asseoir";
-        isPlayerSitting = false;
     }
+
+    private void CameraLock()
+    {
+        if (isPlayerSitting)
+        {
+            playerCamera.DisableRotation();
+
+            // Orientation de la caméra alignée avec la chaise
+            Vector3 forward = transform.forward;
+            forward.y = 0; // on garde l’horizontale
+            forward.Normalize();
+
+            playerCamera.transform.rotation = Quaternion.LookRotation(forward, Vector3.up);
+        }
+        else
+        {
+            playerCamera.EnableRotation();
+        }
+    }
+
 
 }
