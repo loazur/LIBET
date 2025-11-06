4c77fdc6d2db90fa9ce4db5b2a5cfb0788850e31
loazur <clement.bounaix@gmail.com>
2025-11-06 12:27:06 +0100

    Réglage bug voler avec item

--- Assets/Scripts/Player/InteractionSystem/InteractionObjects/S_ItemInteraction.cs
+++ Assets/Scripts/Player/InteractionSystem/InteractionObjects/S_ItemInteraction.cs
@@ -11,6 +11,7 @@ public class S_ItemInteraction : MonoBehaviour, SI_Interactable
     [SerializeField] private float distanceMultiplier; // Distance de l'item quand on le tient
     [SerializeField] private float offsetY; // Position vertical de l'item quant on le tient (0.5 = au milieu de l'ecran)
     private Rigidbody rigidbodyItem;
+    private Collider itemCollider;
     private InputAction dropThrowAction;
     private Transform originalParent; // Utile pour le remettre à son état initial
 
@@ -18,6 +19,7 @@ public class S_ItemInteraction : MonoBehaviour, SI_Interactable
     void Start()
     {
         dropThrowAction = InputSystem.actions.FindAction("CancelInteraction");
+        itemCollider = GetComponent<Collider>();
         originalParent = transform.parent;
         rigidbodyItem = GetComponent<Rigidbody>();
     }
@@ -49,10 +51,9 @@ public class S_ItemInteraction : MonoBehaviour, SI_Interactable
 
     private void PickUp() //& Ramasser un item
     {
-        if (playerInteract.isHoldingItem())
-        {
-            return;
-        }
+        if (playerInteract.isHoldingItem()) return;
+
+        itemCollider.enabled = false; // Pour ne pas voler
 
         rigidbodyItem.useGravity = false;
         rigidbodyItem.isKinematic = true;
@@ -65,10 +66,7 @@ public class S_ItemInteraction : MonoBehaviour, SI_Interactable
 
     private void HoldingItem() //& Gestion lorsqu'on tient un item
     {
-        if (!playerInteract.isHoldingItem())
-        {
-            return;
-        }
+        if (!playerInteract.isHoldingItem()) return;
 
         if (dropThrowAction.WasReleasedThisFrame()) // Action de lacher
         {
@@ -90,26 +88,59 @@ public class S_ItemInteraction : MonoBehaviour, SI_Interactable
     {
         if (!playerInteract.isHoldingItem()) return;
 
+        itemCollider.enabled = true; // On le réactive pour pouvoir detecté l'interaction
+
+        //!Marche pas
+        Vector3 hitPos = castRaycast();
+        Vector3 finalPos = transform.position;
+
+        if (hitPos != Vector3.zero)
+        {
+            finalPos = hitPos + (transform.position - playerCamera.transform.position).normalized * 0.05f;
+        }
+
         rigidbodyItem.useGravity = true;
         rigidbodyItem.isKinematic = false;
         rigidbodyItem.constraints = RigidbodyConstraints.None;
         transform.SetParent(originalParent);
 
+        transform.position = finalPos;
+
         playerInteract.setInteractionEnabled(true);
         playerInteract.setHoldingItem(false);
     }
 
-    
+
     private void Throw() //& Lancer un item
     {
-        if (!playerInteract.isHoldingItem())
-        {
-            return;
-        }
+        if (!playerInteract.isHoldingItem()) return;
 
         //! Lancer
         Debug.Log("Jeter " + interactText);
         playerInteract.setInteractionEnabled(true);
         playerInteract.setHoldingItem(false);
     }
+    
+    private Vector3 castRaycast() //& Retourne la position du raycast si il y a un objet entre l'item et la camera
+    {
+        Vector3 camPos = playerCamera.transform.position;
+        Vector3 itemPos = transform.position;
+
+        Vector3 dir = (itemPos - camPos).normalized;
+        float dist = Vector3.Distance(camPos, itemPos);
+
+        if (Physics.Raycast(camPos, dir, out RaycastHit hit, dist))
+        {
+            if (hit.collider.transform == transform) return Vector3.zero; // Pour pas se détecter lui même
+
+            Debug.DrawRay(camPos, dir * hit.distance, Color.red);
+            Debug.Log("Objet détecté : " + hit.collider.name + " Pos : " + hit.point);
+
+            return hit.point;
+        }
+
+        return Vector3.zero;
+
+    }
+
 }
