a287f3d3456665aa0f76d244dad963b082a9d0d4
Por-Tra <151206590+Por-Tra@users.noreply.github.com>
2025-11-10 13:15:57 +0100

    Refactor S_QuestStateSerializer: enhance documentation for SaveState and LoadState methods; remove unused Start and Update methods.
    Partie Core terminer

--- Assets/Scripts/Quest/Core/S_QuestStateSerializer.cs
+++ Assets/Scripts/Quest/Core/S_QuestStateSerializer.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
 
+
+/**
+ * classe pour sérialiser et désérialiser l'état des quêtes
+ *
+ * @author	Lucas
+ * @since	v0.0.1
+ * @version	v1.0.0	Monday, November 10th, 2025.
+ * @global
+ */
 public class S_QuestStateSerializer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    /**
+     * Méthode pour sauvegarder l'état des quêtes
+     *
+     * @author	Lucas
+     * @since	v0.0.1
+     * @version	v1.0.0	Monday, November 10th, 2025.
+     * @access	public
+     * @param	mixed	quests	
+     * @return	void
+     */
+    public void SaveState(System.Collections.Generic.List<S_QuestInstance> quests)
     {
-        
+        // TODO
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    /**
+     * Charge une sauvegarde
+     *
+     * @var		mixed	LoadState()
+     */
+    public System.Collections.Generic.List<S_QuestInstance> LoadState()
     {
-        
+        return new System.Collections.Generic.List<S_QuestInstance>();
     }
 }
