03afe65cc61ee183fb3fc3b52ae25c6d51a87554
Por-Tra <151206590+Por-Tra@users.noreply.github.com>
2025-11-21 17:12:39 +0100

    Add S_QuestPoint class to represent quest points in the game

---
+++ Assets/Scripts/QuestSystem/S_QuestPoint.cs
@@ -0,0 +1,15 @@
+/**
+ * S_QuestPoint.cs
+ * 
+ * Représente un point de quête dans le jeu.
+ * Chaque point de quête peut être un lieu où le joueur doit se rendre,
+ * interagir avec un objet, ou accomplir une tâche spécifique.
+ * On peut chosir si c'est le début de la qûete ou la fin de la quête.
+**/
+
+using UnityEngine;
+
+public class S_QuestPoint : MonoBehaviour
+{
+    private bool playerIsNear = false;
+}
